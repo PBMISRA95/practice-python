7b896a47bb773d5d261b4abeea74fe2faf3a4760
Priyabrat Mishra <priyabrat.mishra.official@gmail.com>
2025-05-06 17:48:11 +0530

    Refactoring of code by adding function calculator

--- calculator.py
+++ calculator.py
@@ -11,28 +11,30 @@ The calculator supports the following operations:
 """
 # calculator program
 print("\n\nWelcome to the Simple Calculator!")
-status = "run"
+
+def calculator(number1: int, number2: int, option: int) -> int:
+    if option == 1:
+          result = number1 + number2
+    elif option == 2:
+        result = number1 - number2
+    elif option == 3:
+        result = number1 * number2
+    elif option == 4:
+        result = number1 / number2
+    return result
+    
 while True:
     option = 10
     result = -1
     print("\nMenu:\n1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n0. Exit\n")
     option = int(input("Select operator option:  "))
-    if option == 0 or option == 1 or option == 2 or option == 3 or option == 4:
-        if option == 0:
-            break
+
+    if option == 0:
+          break
+    elif option == 1 or option == 2 or option == 3 or option == 4:
         number1 = int(input("Enter first number: "))
         number2 = int(input("Enter second number: "))
-        if option == 1:
-            result = number1 + number2
-        elif option == 2:
-            result = number1 - number2
-        elif option == 3:
-            result = number1 * number2
-        elif option == 4:
-            result = number1 / number2
+        result = calculator(number1, number2, option)
+        print(f"\nThe result of selected operation is: {result}")   
     else:
         print("Incorrect option selected!")
-    print(f"The result of selected operation is: {result}")
-
-
-    
