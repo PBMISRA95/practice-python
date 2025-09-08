8ef548bcb6702c5069ac821d8283138e2dea5c0f
Priyabrat Mishra <priyabrat.mishra.dev@gmail.com>
2025-09-08 12:53:33 +0530

    3rd problem solution

---
+++ play/incedo_questions/3_check_odd_even.py
@@ -0,0 +1,22 @@
+"""
+3. Write a function that takes an integer and returns "even" if it's even, "odd" if it's odd.
+Example: check_even_odd(5) → "odd"
+"""
+
+input_num = int(input("Enter an integer - "))
+
+
+def check_even_odd(input_num: int) -> str:
+    if input_num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+
+print(f"The integer {input_num} is {check_even_odd(input_num)}")
+
+# Logic 2 - Use of ternary operator
+"""
+def check_even_odd(input_num:int) -> str:
+    return "even" if input_num % 2 == 0 else "odd"
+    """
