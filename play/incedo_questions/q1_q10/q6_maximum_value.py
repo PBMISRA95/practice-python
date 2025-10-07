f6e155b1c8e8402e847e5537a867a02dcfded414
Priyabrat Mishra <priyabrat.mishra.dev@gmail.com>
2025-10-07 13:52:02 +0530

    reorganising repo structure

--- play/incedo_questions/q1_q10/q6_maximum_value.py
+++
@@ -1,8 +0,0 @@
-"""
-6. Write a function that takes a list of numbers and returns the maximum value.
-Example - find_max([3, 1, 4, 1, 5]) -> 5
-"""
-
-
-def find_max(numbers: list[int]) -> int:
-    return max(numbers)
